edb47a67f1f62fc17884af6e0b010ee9d05bdfbb
amine372 <nakhila@orange.fr>
2020-12-09 00:45:09 +0100

    Update traitement.php

--- version PHP objet/ignore/traitement.php
+++ version PHP objet/ignore/traitement.php
@@ -1,7 +1,10 @@
 <?php
+require 'Manager_fabricateur.php';
 if(isset($_POST['ok']))
 {
+  $classe = $_POST['classe'];
+  $attributs = $_POST['attributs'];
   $get_event = new Manager_fabricateur;
-  $donnee = $get_event->fabricateur();
+  $donnee = $get_event->fabricateur($classe);
 }
  ?>
