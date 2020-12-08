66ad052a19ed682d6df6101e7a19f01404985aa7
amine372 <nakhila@orange.fr>
2020-12-09 00:02:24 +0100

    Update fabricateur.php

--- version PHP objet/fabricateur.php
+++ version PHP objet/fabricateur.php
@@ -2,13 +2,23 @@
 <html lang="fr">
 <head>
   <meta charset="utf-8">
-  <title>Titre de la page</title>
-  <link rel="stylesheet" href="style.css">
-  <script src="script.js"></script>
+  <title>Fabricateur de Model</title>
 </head>
 <body>
-  ...
-  <!-- Le reste du contenu -->
-  ...
+  <h1>Voici un logiciel révolutionnaire, il fabrique des Model à votre guise</h1>
+  <form action="/ma-page-de-traitement" method="post">
+    <div>
+        <label for="name">Nom :</label>
+        <input type="text" id="name" name="user_name">
+    </div>
+    <div>
+        <label for="mail">e-mail :</label>
+        <input type="email" id="mail" name="user_mail">
+    </div>
+    <div>
+        <label for="msg">Message :</label>
+        <textarea id="msg" name="user_message"></textarea>
+    </div>
+</form>
 </body>
 </html>
