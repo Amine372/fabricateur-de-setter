f64e11d2e83086f56577ef0daaa3d900b82c5096
amine372 <nakhila@orange.fr>
2020-12-09 00:48:06 +0100

    ds

--- version PHP objet/ignore/Manager_fabricateur.php
+++ version PHP objet/ignore/Manager_fabricateur.php
@@ -6,18 +6,10 @@ class Manager_fabricateur
   protected $_classe;
   protected $_attributs;
 
-  public function fabricateur()
+  public function fabricateur($classe,$attributs)
   {
 
-      $bdd = new PDO('mysql:host=localhost;dbname=projet_lycee','root','');
-      $bdd->exec("SET CHARACTER SET utf8");
-      $req = $bdd->prepare('SELECT nom, prenom, id, email, role from  utilisateur;');
-      $req->execute();
-      $donnee = $req->fetchall();
-      if ($donnee)
-      {
-        return $donnee;
-      }
+      $bdd
 
 }
 
--- version PHP objet/ignore/traitement.php
+++ version PHP objet/ignore/traitement.php
@@ -5,6 +5,6 @@ if(isset($_POST['ok']))
   $classe = $_POST['classe'];
   $attributs = $_POST['attributs'];
   $get_event = new Manager_fabricateur;
-  $donnee = $get_event->fabricateur($classe);
+  $donnee = $get_event->fabricateur($classe,$attributs);
 }
  ?>
