3c99289992368c1444311391b3e069fbbbd857a8
amine372 <nakhila@orange.fr>
2020-12-09 13:12:58 +0100

    Update Manager_fabricateur.php

--- version PHP objet/ignore/Manager_fabricateur.php
+++ version PHP objet/ignore/Manager_fabricateur.php
@@ -40,7 +40,11 @@ class Manager_fabricateur
         }
         public function declaration_attributs($classe,$classe_attributs)
         {
-          $cases_tableau = count($classe_attributs)
+          $cases_tableau = count($classe_attributs);
+          for ($i = 1; $i <= 10; $i++)
+          {
+    echo $i;
+}
         }
 
 }
