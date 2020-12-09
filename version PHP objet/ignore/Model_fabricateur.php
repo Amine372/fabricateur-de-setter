f8a20dd6c54f076054c0296d87f7f5f3d471527b
amine372 <nakhila@orange.fr>
2020-12-09 13:24:27 +0100

    Update Model_fabricateur.php

--- version PHP objet/ignore/Model_fabricateur.php
+++ version PHP objet/ignore/Model_fabricateur.php
@@ -1,36 +1,36 @@
 <?php
 class Model_fabricateur
 {
-private $_classe;
-private $_attributs;
+  private $_classe;
+  private $_attributs;
 
-// Liste des getters
-public function classe()
-{
-  return $this->_classe;
-}
-public function attributs()
-{
-  return $this->_attributs;
-}
-// Partie Setter
-public function setclasse($classe)
-{
-$classe = (int) $classe;
+  // Liste des getters
+  public function classe()
+  {
+    return $this->_classe;
+  }
+  public function attributs()
+  {
+    return $this->_attributs;
+  }
+  // Partie Setter
+  public function setclasse($classe)
+  {
+    $classe = (int) $classe;
 
-if ($classe >= 1 && $classe <= 100)
-{
- $this->_classe = $classe;
-}
-}
-public function setattributs($attributs)
-{
-$attributs = (int) $attributs;
+    if ($classe >= 1 && $classe <= 100)
+    {
+      $this->_classe = $classe;
+    }
+  }
+  public function setattributs($attributs)
+  {
+    $attributs = (int) $attributs;
 
-if ($attributs >= 1 && $attributs <= 100)
-{
- $this->_attributs = $attributs;
-}
-}
+    if ($attributs >= 1 && $attributs <= 100)
+    {
+      $this->_attributs = $attributs;
+    }
+  }
 
 }
