b95337febac42bd3f2b9fe40250e1d4edb2d9c1d
Amine372 <nakhila@orange.fr>
2021-02-02 15:44:12 +0100

    ;: ;:

---
+++ fournitures/noguez.php
@@ -0,0 +1,47 @@
+<?php
+class Model_noguez
+{
+private $_h;
+private $_g;
+private $_h;
+// Liste des getters
+public function h()
+{
+  return $this->_h;
+}
+public function g()
+{
+  return $this->_g;
+}
+public function h()
+{
+  return $this->_h;
+}
+// Partie Setter
+public function seth($h)
+{
+$h = (int) $h;
+
+if ($h >= 1 && $h <= 100)
+{
+ $this->_h = $h;
+}
+}
+public function setg($g)
+{
+$g = (int) $g;
+
+if ($g >= 1 && $g <= 100)
+{
+ $this->_g = $g;
+}
+}
+public function seth($h)
+{
+$h = (int) $h;
+
+if ($h >= 1 && $h <= 100)
+{
+ $this->_h = $h;
+}
+}
